5a03308d3417b8a64e03b548797503633707ac6a
douglas <carlosdouglas@alu.ufc.br>
2024-03-26 22:15:13 -0300

    update MainActivity

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -6,26 +6,55 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Switch
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    var percentual:Double = 0.7
+    private var percentual:Double = 0.7
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i("PDM24.1","No onCreate, $percentual")
 
-        percentual=0.7
         val btCalc: Button = findViewById(R.id.btCalcular)
-        val textMsg:TextView= findViewById(R.id.textMsg)
+        val textMsg:TextView = findViewById(R.id.textMsg)
+        val switchButton = findViewById<Switch>(R.id.swPercentual)
+        val precoAlcool:Double = findViewById<TextView?>(R.id.text_alcool).toString().toDouble()
+        val precoGasolina:Double = findViewById<TextView?>(R.id.text_gasolina).toString().toDouble()
+
         //btCalc.setBackgroundColor(Color.CYAN)
         btCalc.setOnClickListener(View.OnClickListener {
             //código do evento
-            percentual=0.75
-            textMsg.text="Já tenho o percentual"
+            val switchState = switchButton.isChecked
+            if (switchState) {
+                percentual = 0.75
+                mostrarMsgvaleApenaAlcool(valeApenaAlcool(percentual, precoAlcool, precoGasolina), textMsg)
+
+            } else {
+                percentual = 0.7
+                mostrarMsgvaleApenaAlcool(valeApenaAlcool(percentual, precoAlcool, precoGasolina), textMsg)
+
+            }
+
             Log.d("PDM24","No btCalcular, $percentual")
         })
     }
+
+ private fun valeApenaAlcool(percentual:Double, precoAlcool:Double,precoGasolina:Double ):Boolean{
+     if (precoAlcool <= percentual * precoGasolina){
+         return true;
+     }
+     return false;
+ }
+
+private fun mostrarMsgvaleApenaAlcool(estado:Boolean, textMsg:TextView){
+    if (estado){
+        textMsg.text = "Vale apena usar alcool"
+    }else{
+        textMsg.text = "Não vale apena usar alcool"
+    }
+}
+
 override fun onResume(){
     super.onResume()
     Log.d("PDM24","No onResume, $percentual")
